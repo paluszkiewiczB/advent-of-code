db78537824cb221d7543eacc5b04f31fd240b78c
Bartosz Paluszkiewicz <9494671+paluszkiewiczB@users.noreply.github.com>
2022-12-02 18:24:31 +0100

    feat(02): part one done

--- 02/main.go
+++ 02/main.go
@@ -1,5 +1,102 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type shape = string
+
+const (
+	rock     shape = "rock"
+	paper    shape = "paper"
+	scissors shape = "scissors"
+)
+
+type move struct {
+	opponent, my shape
+}
+
+var (
+	shapePoints = map[shape]int{
+		rock:     1,
+		paper:    2,
+		scissors: 3,
+	}
+)
+
 func main() {
-	println("hello world")
+	m := readMoves()
+	fmt.Printf("part one points: %d", partOne(m))
+}
+
+func partOne(m []move) int {
+	var points int
+	for _, move := range m {
+		points += shapePoints[move.my]
+		points += calculateMatchPoints(move)
+	}
+
+	return points
+}
+
+func calculateMatchPoints(m move) int {
+	if m.my == m.opponent {
+		return 3
+	}
+
+	if m.opponent == rock && m.my == paper {
+		return 6
+	}
+
+	if m.opponent == scissors && m.my == rock {
+		return 6
+	}
+
+	if m.opponent == paper && m.my == scissors {
+		return 6
+	}
+
+	return 0
+}
+
+var opponentMoves = map[uint8]shape{
+	'A': rock,
+	'B': paper,
+	'C': scissors,
+}
+var myMoves = map[uint8]shape{
+	'X': rock,
+	'Y': paper,
+	'Z': scissors,
+}
+
+func readMoves() []move {
+	file, err := os.Open("input.txt")
+	defer file.Close()
+	must(err)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	moves := make([]move, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		opponent, ook := opponentMoves[line[0]]
+		my, mok := myMoves[line[2]]
+		if !ook || !mok {
+			panic(fmt.Errorf("could not parse move: %s. found values: %s, %s", line, opponent, my))
+		}
+		move := move{opponent: opponent, my: my}
+		moves = append(moves, move)
+	}
+
+	return moves
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
