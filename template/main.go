03a9a5220315608fd98399a912195f96db5cf4c7
Bartosz Paluszkiewicz <9494671+paluszkiewiczB@users.noreply.github.com>
2022-12-04 07:13:55 +0100

    chore(setup-day): improve template

--- template/main.go
+++ template/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 func main() {
 	println("hello world")
+}
+
+func readInput() (s *bufio.Scanner, closeFunc func() error) {
+	file, err := os.Open("sample-input.txt")
+	must(err)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	return scanner, file.Close
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
