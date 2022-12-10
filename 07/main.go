a21c5b8e979583ae683268e87115fe84da72b638
Bartosz Paluszkiewicz <9494671+paluszkiewiczB@users.noreply.github.com>
2022-12-10 01:01:21 +0100

    feat(07): solve part one

--- 07/main.go
+++ 07/main.go
@@ -4,21 +4,124 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("part one: %s\n", partOne())
-	fmt.Printf("part two: %s\n", partTwo())
+	fmt.Printf("part one: %d\n", partOne())
+	//fmt.Printf("part two: %s\n", partTwo())
 }
 
-func partOne() string {
+type dir struct {
+	name string
+	size int
+}
+
+func partOne() int {
 	c := make(chan string)
-	go readInput(c, "sample-input.txt")
-	for s := range c {
-		println(s)
+	go readInput(c, "input.txt")
+
+	root := parseCd(<-c)
+
+	fsC := make(chan dir)
+	fs := make(map[string]int)
+	go func() {
+		for d := range fsC {
+			if ex, ok := fs[d.name]; ok {
+				fs[d.name] = ex + d.size
+			} else {
+				fs[d.name] = d.size
+			}
+		}
+	}()
+
+	parseDir(c, fsC, root)
+	close(fsC)
+
+	var total int
+	for _, size := range fs {
+		if size <= 100000 {
+			total += size
+		}
 	}
 
-	return "todo"
+	return total
+}
+
+func parseDir(c chan string, fs chan dir, wd string) {
+	relativize := childOf(wd)
+	for line := range c {
+		if strings.HasPrefix(line, "$ ls") {
+			line = parseLs(c, fs, wd)
+		}
+
+		if strings.HasPrefix(line, "$ cd ") {
+			to := parseCd(line)
+
+			if to == ".." {
+				return
+			}
+
+			parseDir(c, fs, relativize(to))
+		}
+	}
+}
+
+func parseLs(c chan string, fs chan dir, wd string) string {
+	relativize := childOf(wd)
+	var dirSize int
+	for line := range c {
+		if strings.HasPrefix(line, "dir ") {
+			d := strings.Split(line, "dir ")[1]
+			fs <- dir{name: relativize(d)}
+			continue
+		}
+
+		if strings.HasPrefix(line, "$") {
+			fs <- dir{name: wd, size: dirSize}
+			for _, parent := range parentDirs(wd) {
+				fs <- dir{name: parent, size: dirSize}
+			}
+			return line
+		}
+
+		regularFile := strings.Split(line, " ")
+		size, err := strconv.Atoi(regularFile[0])
+		must(err)
+		dirSize += size
+	}
+
+	fs <- dir{name: wd, size: dirSize}
+	for _, parent := range parentDirs(wd) {
+		fs <- dir{name: parent, size: dirSize}
+	}
+	return ""
+}
+
+func parentDirs(dir string) []string {
+	out := make([]string, 0)
+	for {
+		dir = filepath.Dir(dir)
+		if dir == "/" {
+			return out
+		}
+		out = append(out, dir)
+	}
+}
+
+func childOf(parent string) func(c string) string {
+	return func(c string) string {
+		if parent == "/" {
+			return parent + c
+		}
+		return parent + "/" + c
+	}
+}
+
+func parseCd(cd string) string {
+	return strings.Split(cd, "$ cd ")[1]
 }
 
 func partTwo() string {
