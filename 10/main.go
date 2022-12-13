87329977886d13d785c98c9eeaf2a9968e9061c0
Bartosz Paluszkiewicz <9494671+paluszkiewiczB@users.noreply.github.com>
2022-12-13 01:25:37 +0100

    feat(10): solve part one

--- 10/main.go
+++ 10/main.go
@@ -4,21 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("part one: %s\n", partOne())
-	fmt.Printf("part two: %s\n", partTwo())
+	fmt.Printf("part one: %d\n", partOne())
+	//fmt.Printf("part two: %s\n", partTwo())
 }
 
-func partOne() string {
+func partOne() int {
 	c := make(chan string)
-	go readInput(c, "sample-input.txt")
-	for s := range c {
-		println(s)
+	go readInput(c, "input.txt")
+
+	xC := make(chan int)
+	sum := make(chan int)
+	go func() {
+		s := 0
+		for v := range xC {
+			s += v
+		}
+		sum <- s
+	}()
+
+	interestingCycles := map[int]struct{}{20: {}, 60: {}, 100: {}, 140: {}, 180: {}, 220: {}}
+	cycle, x := 1, 1
+	nextCycle := func() {
+		if _, ok := interestingCycles[cycle]; ok {
+			fmt.Printf("interesting cycle number: %d, value: %d\n", cycle, x)
+			xC <- x * cycle
+		}
+		cycle++
 	}
 
-	return "todo"
+	for s := range c {
+		if s == "noop" {
+			nextCycle()
+			continue
+		}
+
+		add := strings.Split(s, " ")
+		v, err := strconv.Atoi(add[1])
+		must(err)
+		nextCycle()
+		nextCycle()
+		x += v
+	}
+
+	close(xC)
+	return <-sum
 }
 
 func partTwo() string {
